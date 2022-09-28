e4f223b88b81c2d55cc9cdcf07c3d3414e682d0a
GrangerVMV <javitrunk@hotmail.com>
2022-09-28 07:32:05 +0200

    Tercer ejemplo: Clases, constructores y sobrecarga de constructores

--- Program.cs
+++ Program.cs
@@ -22,6 +22,14 @@ namespace CursoCPractica6_POO
             obj.cambiaValorEuro(2); // Modifica el valor de euro mediante método de acceso. 
             //obj.cambiaValorEuro(-7.850); // No permite el uso de valores negativos
             Console.WriteLine(obj.convierte(50));
+
+            // Tercer ejemplo: Clases, constructores y sobrecarga de constructores
+            Coche miCoche1 = new Coche(); //Creamos un objeto coche (instancia de la clase Coche)
+            Console.WriteLine(miCoche1.getInfoCoche());
+            Coche miCoche2 = new Coche(); // Podemos crear tantas instancias de la clase coche como queramos 
+            Console.WriteLine(miCoche2.getInfoCoche());// Imprime lo mismo que en el caso anterior porque el constructor define parámetros por defecto fijos
+            Coche miCoche3 = new Coche(5325.50, 2200.25); //Creamos una nueva instancia esta vez accediendo al segundo constructor
+            Console.WriteLine(miCoche3.getInfoCoche());
         }
     }
 
@@ -49,5 +57,31 @@ namespace CursoCPractica6_POO
         }
     }
 
+    class Coche // Tercer ejemplo: Clases, constructores y sobrecarga de constructores
+    {
+        public Coche() //Esto es un constructor (valores por defecto de la clase Coche)
+        {
+            ruedas = 4;
+            largo = 3400.25;
+            ancho = 1800.75;
+        }
 
+        public Coche(double largoCoche, double anchoCoche) // Con este constructor estaríamos creando una sobre carga de constructores. En este nos permite modificar los valores por defecto de la clase
+        {
+            ruedas = 4;
+            largo = largoCoche;
+            ancho = anchoCoche;
+        }
+
+        public string getInfoCoche() //Método de acceso para poder visualizar los atributos de la clase (ya que son private)
+        {
+            return "Info del coche:\nRuedas:" + ruedas + " Largo:" + largo + " Ancho:" + ancho;
+        }
+        
+        private int ruedas;
+        private double largo;
+        private double ancho;
+        private bool climatizador;
+        private string tapiceria;
+    }
 }
